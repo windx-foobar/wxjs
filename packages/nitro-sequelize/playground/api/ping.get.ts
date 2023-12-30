be727086a27778e7b815d59b75bfd57a0e985b18
windx-foobar <bezalkogoln1ycoder@gmail.com>
2023-12-30 04:01:00 +0300

    small fixes

--- packages/nitro-sequelize/playground/api/ping.get.ts
+++ packages/nitro-sequelize/playground/api/ping.get.ts
@@ -6,6 +6,7 @@ export default defineEventHandler(async (event) => {
 
   const nitropackPackage = await import('nitropack/package.json');
   const nitroSequelizePackage = await import('../../package.json');
+  const h3SequelizePackage = await import('@wxjs/h3-sequelize/package.json');
   const wxjsSequelizePackage = await import('@wxjs/sequelize/package.json');
   const [sqliteResult] = await sequelize.query('SELECT sqlite_version();', {
     type: QueryTypes.SELECT
@@ -16,6 +17,7 @@ export default defineEventHandler(async (event) => {
     stack: {
       nitropack: nitropackPackage.version,
       '@wxjs/sequelize': wxjsSequelizePackage.version,
+      '@wxjs/h3-sequelize': h3SequelizePackage.version,
       '@wxjs/nitro-sequelize': nitroSequelizePackage.version,
       'sqlite': sqliteResult['sqlite_version()']
     }
